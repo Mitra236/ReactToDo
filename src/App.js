833347d6a04133c63389b91b49c95cac79900b53
Mitra Pantic <mitra196506@gmail.com>
2019-10-21 19:32:39 +0200

    Added todo options

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Todos from './ToDos';
+import AddTodo from './AddForm'
 
+class App extends Component {
+  state = {
+    todos: [
+       {id: 1, content: 'buy some milk'},
+       {id: 2, content: 'do some work'}
+      ]
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  deleteToDo = (id) => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id
+    });
+    this.setState({
+      todos: todos
+    })
+  }
+
+  addTodo = (todo) => {
+    this.id = Math.random()
+    //spread operator to get each individual item from todos list ...
+    //, we are creating a new array and we are dumping the content of existing todos array
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
+
+  render() {
+    return (
+      <div className = "todo-app container">
+        <h1 className = 'center blue-text'>Todo's</h1>
+        <Todos todos= {this.state.todos} deleteToDo = {this.deleteToDo}/>
+        <AddTodo addTodo = {this.addTodo}></AddTodo>
+      </div>
+    )
+  }
 }
 
 export default App;
